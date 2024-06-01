bcd5c8de1758e8ac45eba00e86d5a8561a9054d8
juanpablotr14 <infobethlemitastascon@gmail.com>
2024-05-31 20:48:43 -0500

    Model Libro

--- libros/src/main/java/com/servicios/libros/models/Libro.java
+++ libros/src/main/java/com/servicios/libros/models/Libro.java
@@ -1,5 +1,26 @@
 package com.servicios.libros.models;
 
-public class Libro {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
 
+@Builder
+@Entity
+@Table(name = "\"libro\"")
+public class Libro {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@Column( name = "titulo" )
+	private String titulo;
+	
+	@Column( name = "autor" )
+	private String autor;
+	
 }
