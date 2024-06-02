623bbb380398402c9e8aa0ab411e12bcc0a4b9ec
juanpablotr14 <infobethlemitastascon@gmail.com>
2024-06-02 14:16:00 -0500

    Create Gateway

--- clientes/src/main/java/com/servicios/clientes/ClientesApplication.java
+++ clientes/src/main/java/com/servicios/clientes/ClientesApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan({ "com.servicios.clientes", "com.servicios.CommonLibrary.models" })
-@EnableJpaRepositories({ "com.servicios.clientes.repository", "com.servicios.CommonLibrary.models" })
-@EntityScan({ "com.servicios.clientes.entity", "com.servicios.CommonLibrary.models" })
+@ComponentScan({ "com.servicios.clientes" })
+@EnableJpaRepositories({ "com.servicios.clientes.repository" })
+@EntityScan({ "com.servicios.CommonLibrary.models" })
 @SpringBootApplication
 public class ClientesApplication {
 
